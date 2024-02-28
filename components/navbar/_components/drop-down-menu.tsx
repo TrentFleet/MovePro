a7989b42eb371584cd446ceccfd3efa0516bf91c
TrentFleet <Trent.o.fleet@gmail.com>
2024-02-28 15:43:41 +1300

    auth 1

--- components/navbar/_components/drop-down-menu.tsx
+++ components/navbar/_components/drop-down-menu.tsx
@@ -176,11 +176,11 @@ const DropDownMenu: React.FC<DropDownMenuProps> = ({ onClose }) => {
           <div className="font-thin lg:flex items-center hidden">|</div>
           <div className="flex lg:space-x-4 items-center pr-4">
             <div>
-              <Link href="/Sign-In">
+              <LoginLink href="/Sign-In">
                 <Button variant={"outline"} className="p-4 mt-4">
                   Log in
                 </Button>
-              </Link>
+              </LoginLink>
             </div>
           </div>
         </Accordion>
