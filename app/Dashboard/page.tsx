b7da60d9198ef8704d53f58c78417448699a51e7
TrentFleet <Trent.o.fleet@gmail.com>
2024-02-29 19:58:23 +1300

    update

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -3,11 +3,12 @@ import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="lg:flex lg:flex-col lg:mx-20 sm:flex-col gap-10 justify-center items-center">
+       <h1>You made it to the Dashboard Page! coming soon.</h1>
+       
         <LogoutLink>
             <Button>Log out</Button>
             </LogoutLink>
-      <h1>Dashboard</h1>
     </div>
   );
 }
