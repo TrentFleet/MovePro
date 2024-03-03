a11b638dc6efafe9d624e025529e7e7ed0227b8a
TrentFleet <Trent.o.fleet@gmail.com>
2024-03-03 22:35:52 +1300

    final push

--- components/InstructionSection.tsx
+++ components/InstructionSection.tsx
@@ -12,22 +12,22 @@ const InstructionSection = () => {
     <div className="m-6">
       {isContentVisible && (
         <div className="flex-col w-64 mx-auto px-auto flex justify-center items-center font-semibold text-md m-4">
-  <h1 className="m-2">
-    1. Create your personalized workout using the options below.
-  </h1>
-  <h1 className="text-purple-600 m-2">
-    2. Pick your warm-up and stretches for a customized routine.
-  </h1>
-  <h1 className="m-2">
-    3. Select your training type, muscle group, and intensity.
-  </h1>
-  <h1 className="text-purple-600 m-2">
-    4. Click Generate Workout and get ready to crush it!
-  </h1>
-</div>
+          <h1 className="m-2">
+            1. Create your personalized workout using the options below.
+          </h1>
+          <h1 className="text-purple-600 m-2">
+            2. Pick your warm-up and stretches for a customized routine.
+          </h1>
+          <h1 className="m-2">
+            3. Select your training type, muscle group, and intensity.
+          </h1>
+          <h1 className="text-purple-600 m-2">
+            4. Click Generate Workout and get ready to crush it!
+          </h1>
+        </div>
       )}
       <Button onClick={toggleContentVisibility}>
-        {isContentVisible ? "Hide Instructions" : "Show Instructions"}
+        {isContentVisible ? 'Hide Instructions' : 'Show Instructions'}
       </Button>
     </div>
   );
